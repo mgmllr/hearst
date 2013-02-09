ac74ce6550d3fd9b20a1d60fa9729172a8eb9e2c
ashley williams <heyashleyashley@gmail.com>
2013-02-08 19:27:26 -0500

    add bootstrap js. add singly. update layout for singly instagram and twitter signin. instagram photo example

--- app.rb
+++ app.rb
@@ -1,7 +1,11 @@
 require 'rubygems'
 require 'bundler'
 
+SINGLY_API_BASE = "https://api.singly.com"
+
 Bundler.require
+
+enable :sessions
 
 class ExcellentRussianApp < Sinatra::Application
   configure :development do
@@ -21,12 +25,51 @@ class ExcellentRussianApp < Sinatra::Application
     set :public_folder, 'public'
   end
 
+  use OmniAuth::Builder do
+    provider :singly, '21ff03d9e1e42e28c5c53e94c8c36208', '942d969f6a6963afb85a7aa68b21bedd'
+  end
+
+  helpers do 
+    def auth_path(service)
+      url = "/auth/singly?service=#{service}"
+      url += "&access_token#{session[:access_token]}" if session[:access_token]
+      url
+    end
+  end
+
   get '/' do
+    if session[:access_token]
+    @profiles = HTTParty.get(profiles_url, {
+      :query => {:access_token => session[:access_token]}
+      }).parsed_response
+    @photos = HTTParty.get(photos_url, {
+      :query => {:access_token => session[:access_token]}
+      }).parsed_response
+    end
     haml :index
   end
 
   get '/stylesheet.css' do
     scss :styles
+  end
+
+  get "/auth/singly/callback" do
+    auth = request.env["omniauth.auth"]
+    session[:access_token] = auth.credentials.token
+    redirect "/"
+  end
+
+  get "/logout" do
+    session.clear
+    redirect "/"
+  end
+
+  def profiles_url
+    "#{SINGLY_API_BASE}/profiles"
+  end
+
+  def photos_url
+    "#{SINGLY_API_BASE}/types/photos"
   end
 end
 
