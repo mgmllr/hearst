c6681ef5459d8f01e9473f3a995fe594947f356d
Michael Hansen <modality2@gmail.com>
2013-02-10 07:00:50 -0500

    scoring tweaks

--- lib/trend.rb
+++ lib/trend.rb
@@ -47,27 +47,28 @@ class Trend < Model
   end
 
   def self.calculate_timeliness(post, trend)
-    trend_count = trend[:mentions].count
+    trend_count = trend[:total_mentions]
     mentions = trend[:mentions].sort {|a,b| a <=> b }
 
+    points = 0
     post_index = 0
     mentions.each do |mention|
       post_index += 1 if mention < post[:timestamp]
     end
 
     if post_index == 0
-      points = 10
+      points += 10
     else
-      quotient = (tweet_index.to_f / trend_count.to_f)
+      quotient = (post_index.to_f / trend_count.to_f)
       case 
       when quotient <= 0.1
-          points = 7.5
+        points += 7.5
       when quotient > 0.1 && quotient <= 0.25
-          points = 5
+        points += 5
       when quotient > 0.25 && quotient <= 0.5
-          points = 2.5
+        points += 2.5
       when quotient > 0.5
-          points = 1
+        points += 1
       end
     end
     points * trend_count
