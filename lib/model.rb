be5cce3e33c0460c4a2d930819540c59b6cbc805
Michael Hansen <modality2@gmail.com>
2013-02-10 05:54:03 -0500

    what

--- lib/model.rb
+++ lib/model.rb
@@ -1,6 +1,11 @@
 class Model
   def self.redis
     uri = URI.parse(ENV["REDISCLOUD_URL"] || "http://localhost:6379")
+  
+    puts "*******************************"
+    puts "initializing redis client"
+    puts uri
+
     @redis ||= Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
   end
 end
