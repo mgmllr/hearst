5a828bf991e13f69d408de58d012c335c84bc1fd
Max Miller <maxwellgmiller@gmail.com>
2013-02-09 22:41:25 -0500

    Fix Hearst articles parsing

--- app.rb
+++ app.rb
@@ -76,7 +76,10 @@ class ExcellentRussianApp < Sinatra::Application
   end
 
   get "/hearst" do
-    @articles = HTTParty.get(articles_url, {:query=> {:keywords=> "fashion", :api_key=>"nvp2n7m2b6stwn3xha8m4ype"}})
+    json = StringIO.new(HTTParty.get(articles_url, {:query=> {:keywords=> "fashion", :api_key=>"nvp2n7m2b6stwn3xha8m4ype"}}))
+    parser = Yajl::Parser.new
+    @articles = parser.parse(json)
+
     haml :hearst
   end
 
