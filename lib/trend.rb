0acc466fcbac0c99de4db9aad0761982eae42358
Max Miller <maxwellgmiller@gmail.com>
2013-02-10 02:52:25 -0500

    Initial scoring algorithm

--- lib/trend.rb
+++ lib/trend.rb
@@ -37,4 +37,40 @@ class Trend
     trends
   end
 
+  def self.score_for_post(object)      
+    shares = object[:shares]
+    trend = self.get_trend "#{object[:keyword]}" ##Make sure this is the right symbol!!!
+    score = calculate_timeliness(object, trend) * shares
+    score
+  end
+
+  def calculate_timeliness(object, trend)
+    if ##twitter
+      object_time = Time.parse object["data"]["created_at"]
+    if ##instagram
+      object_time = Time.at(object["data"]["created_time"])
+    trend_start = trend.first
+    trend_count = trend[:mentions].count
+
+    mentions = trend[:mentions].sort {|a,b| a < b }
+
+    object_index = trend[:mentions].index(object_time) + 1
+    if object_index == 1
+      points = 1
+    else
+      case (tweet_index.to_f / trend_count.to_f)
+      when <= 0.1
+          points = 0.75
+      when > 0.1 && <= 0.25
+          points = 0.5
+      when > 0.25 && <= 0.5
+          points = 0.25
+      when > 0.5
+          points = 0.1
+      end
+    end
+    points
+  end
+
+
 end
