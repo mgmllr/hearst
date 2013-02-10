008a98131b9af503e8b8318a5bbe3e1f45e44a86
Michael Hansen <modality2@gmail.com>
2013-02-10 03:42:31 -0500

    bug fixin, post data

--- lib/post.rb
+++ lib/post.rb
@@ -2,7 +2,7 @@ require 'redis/hash_key'
 
 class Post
   def self.add_post(user_id, post_data)
-    post = Redis::HashKey.new("users:#{user_id}:post:#{post_data["id"]}")
+    post = Redis::HashKey.new("users:#{user_id}:post:#{post_data[:id]}")
     post.bulk_set(post_data)
   end
 end
--- script/get_user_data.rb
+++ script/get_user_data.rb
@@ -32,30 +32,38 @@ users.each do |user|
       data = status["data"]
       keyword_array = data["entities"]["hashtags"].map {|ht| ht["text"] }
       keyword_string = keyword_array.join(',')
+      timestamp = Time.parse(data["created_at"]).to_i
+
       Post.add_post(profile.parsed_response["id"], {
-          :id => data["id"],
-          :post_url => "http://twitter.com/#{data["user"]["screen_name"]}/status/#{data["id"]}",
-          :timestamp => data["created_at"],
-          :amps => data["retweet_count"],
-          :content => data["text"],
-          :keywords => keyword_string,
-          :img_url => data["media"] ? data["media"]["media_url"] : nil
+        :id => data["id"],
+        :post_url => "http://twitter.com/#{data["user"]["screen_name"]}/status/#{data["id"]}",
+        :timestamp => timestamp,
+        :amps => data["retweet_count"],
+        :content => data["text"],
+        :keywords => keyword_string,
+        :img_url => data["media"] ? data["media"]["media_url"] : nil
       })
       end
   end
   if user["instagram"] == "true"
-    grams = HTTParty.get(twitter_url, {:query=> {:access_token=> user["access_token"]}})
+    grams = HTTParty.get(instagram_url, {:query=> {:access_token=> user["access_token"]}})
     grams.each do |gram|
-      amps = gram["comments"]["count"] + gram["likes"]["count"]
-      keyword_string = status["data"]["tags"].join(',')
+      data = gram["data"]
+      amps = 0
+      amps += data["comments"]["count"] if data["comments"]
+      amps += data["likes"]["count"] if data["likes"]
+
+      keyword_string = data["tags"].join(',')
+      caption = data["caption"]["text"] if data["caption"]
+
       Post.add_post(profile.parsed_response["id"], {
-          :id => gram["id"],
-          :post_url => gram["link"],
-          :timestamp => gram["created_time"],
-          :amps => amps,
-          :content => gram["caption"]["text"],
-          :keywords => keyword_string,
-          :img_url => gram["images"]["standard_resolution"]["url"]
+        :id => gram["id"],
+        :post_url => data["link"],
+        :timestamp => data["created_time"],
+        :amps => amps,
+        :content => caption,
+        :keywords => keyword_string,
+        :img_url => data["images"]["standard_resolution"]["url"]
       })
     end
   end
